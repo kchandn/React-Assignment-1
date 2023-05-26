289e2570a49d9a9b0a690206263ba770ba92c740
Chandan.kumar <chandan.kumar@trivium-esolutions.com>
2023-05-26 17:20:46 +0530

    Alert showing and hiding.

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,19 +1,25 @@
-import React from "react";
+import { useState } from "react";
+import Alert from "./Alert";
 
 interface Props {
   buttonName: string;
 }
 
 const Button = (props: Props) => {
+  const [alert, setAlert] = useState(false);
   return (
-    <button
-      type="button"
-      className="btn btn-primary"
-      id="liveAlertBtn"
-      onClick={() => console.log("Button clicked!")}
-    >
-      {props.buttonName}
-    </button>
+    <>
+      <button
+        type="button"
+        className="btn btn-primary"
+        id="liveAlertBtn"
+        onClick={() => setAlert(!alert)}
+      >
+        {props.buttonName}
+      </button>
+      <div></div>
+      {alert ? <Alert /> : ""}
+    </>
   );
 };
 
